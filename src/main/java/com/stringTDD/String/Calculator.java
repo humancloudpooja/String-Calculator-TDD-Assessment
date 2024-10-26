43f6aa34f137d70935a2b42d7f21d9fa0413240d
Pooja Narale <pooja.narale@humancloud.co.in>
2024-10-26 11:41:33 +0530

    Test Case Pass For Adding Empty String

---
+++ src/main/java/com/stringTDD/String/Calculator.java
@@ -0,0 +1,16 @@
+package com.stringTDD.String;
+
+public class Calculator {
+
+    public static int add(String text)
+    {
+        if(!text.equals(""))
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
