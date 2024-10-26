f63624297a336841561b17e0f58110475f45535e
Pooja Narale <pooja.narale@humancloud.co.in>
2024-10-26 11:40:14 +0530

    Test Case Fail For Adding Empty String

--- src/test/java/com/stringTDD/String/StringApplicationTests.java
+++ src/test/java/com/stringTDD/String/StringApplicationTests.java
@@ -3,11 +3,16 @@ package com.stringTDD.String;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class StringApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testEmptyString()
+	{
+		assertEquals(0,Calculator.add(""));
 	}
 
-}
+} src/test/java/com/stringTDD/String/StringApplicationTests.java
+
